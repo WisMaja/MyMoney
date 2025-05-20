59c56504dc373fc9ea8b1a79e5427f96903d398a
Maja <wisniewska.maja03@gmail.com>
2025-05-20 12:22:31 +0200

    Tworzenie domyślnego portfela podczas rejestracji

---
+++ api/Controllers/UpdateCategoryDto.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace api.Dtos.Category
+{
+    public class UpdateCategoryDto
+    {
+        [Required, MaxLength(100)]
+        public string Name { get; set; }
+    }
+}
